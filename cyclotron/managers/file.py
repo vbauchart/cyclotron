23386ab16d3c402bb1c2afe1117fa23bd8898611
Vincent Bauchart <bauchart@bvassociates.fr>
2018-07-17 18:04:28 +0200

    file manager handle only one file

--- cyclotron/managers/file.py
+++ cyclotron/managers/file.py
@@ -2,11 +2,10 @@ from shutil import copy, move
 from os.path import isdir, dirname, basename, join, abspath, isfile
 from cyclotron.managers import CriticalOperationException
 import logging
+from glob import glob
+import os
 
 logger = logging.getLogger(__name__)
-
-
-class XFile
 
 
 class FileManager(object):
@@ -14,13 +13,42 @@ class FileManager(object):
     dirname = None
     basename = None
 
-    def __init__(self, path):
-        self.full_path = abspath(path)
+    def __init__(self, glob_pattern, pick_order='filename'):
+
+        self.glob_pattern = glob_pattern
+
+        self.pick_order = pick_order
+
+        self.full_path = self.pick_file()
+
         self.dirname = dirname(self.full_path)
         self.basename = basename(self.full_path)
 
         if not isfile(self.full_path):
             raise CriticalOperationException('%s is not a valid file' % self.full_path)
+
+    def pick_file(self):
+
+        file_list = glob(self.glob_pattern)
+        if len(file_list) == 0:
+            raise CriticalOperationException('%s : no matching file' % self.glob_pattern)
+        elif len(file_list) == 1:
+            return file_list[0]
+        else:
+            if self.pick_order == 'filename':
+                file_list.sort(reverse=True)
+            elif self.pick_order == 'mtime':
+                file_list.sort(key=os.path.getctime, reverse=True)
+            else:
+                raise Exception('%s is not a valid pick strategy' % pick_order)
+
+            for file in file_list:
+                logger.debug('%s is a candidate for %s' % (file, self.glob_pattern))
+
+            found_file = file_list[0]
+            logger.info('%s selected for pattern %s' % (file, self.glob_pattern))
+
+            return found_file
 
     def copy_into(self, destination):
         if not isdir(destination):
